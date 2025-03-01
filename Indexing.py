6e561a84a3dd1af656fff0e4c7615123af5e74d9
shre-coder <2021ai68sh@mitsgwl.ac.in>
2025-03-01 23:43:51 +0530

    Indexing and Slicing

---
+++ Indexing.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+var = np.array([9, 8, 7, 6])
+#               0,1,2,3
+#               -4 -3 -2 -1
+
+# print(var[1])
+# print(var[-3])
+
+var1 = np.array([[9, 8, 7], [4, 5, 6]])
+
+# print(var1)
+# print(var1.ndim)
+# print(var1[0, 2])
+
+var2 = np.array([[[1, 2], [1, 5]]])
+
+# print(var2)
+# print(var2.ndim)
+# print(var2[0, 1, 1])
